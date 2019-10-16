e828303ec949f7b3e48cbfae4bca32ed010e1231
Aditya Sanil <aditya.sanil@somaiya.edu>
2019-10-16 16:37:27 +0530

    Search implemented
    partial matching & fuzzy remaining

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 
 const style = {
   inputStyle: {
@@ -16,47 +14,20 @@ const style = {
     },
 
     fontSize: "20px"
-  },
-
-  paperStyle: {
-    width: "95%",
-    height: "30px",
-    marginLeft: "25px",
-    marginBottom: "10px",
-    marginTop: "20px",
-
-    "@media only screen and (max-width: 600px)": {
-      width: "85%",
-      marginLeft: "20px",
-      height: "20px",
-      marginBottom: "33px",
-      marginTop: "6px"
-    }
   }
 };
 
 class SearchBox extends Component {
-  state = {
-    query: ""
-  };
-
-  handleChange = e => {
-    const query = e.target.value;
-    this.setState({ query });
-  };
-
   render() {
     const { classes } = this.props;
 
     return (
-      <Paper className={classes.paperStyle}>
-        <input
-          placeholder="Search"
-          value={this.state.query}
-          onChange={this.handleChange}
-          className={classes.inputStyle}
-        />
-      </Paper>
+      <input
+        placeholder="Search..."
+        value={this.props.value}
+        onChange={this.props.onChange}
+        className={classes.inputStyle}
+      />
     );
   }
 }
--- src/components/searchPage.jsx
+++ src/components/searchPage.jsx
@@ -1,20 +1,93 @@
 import React, { Component } from "react";
 import SearchBox from "./searchBox";
 import SearchCard from "./searchCard";
+import http from "../services/httpService";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const style = {
+  paperStyle: {
+    width: "95%",
+    height: "30px",
+    marginLeft: "25px",
+    marginBottom: "10px",
+    marginTop: "20px",
+
+    "@media only screen and (max-width: 600px)": {
+      width: "85%",
+      marginLeft: "20px",
+      height: "20px",
+      marginBottom: "33px",
+      marginTop: "6px"
+    }
+  }
+};
 
 class SearchPage extends Component {
-  state = {};
+  state = {
+    query: "",
+    user: []
+  };
+
+  handleChange = e => {
+    const query = e.target.value;
+    this.searchQuery(query);
+    this.setState({ query });
+  };
+
+  searchQuery = async query => {
+    try {
+      console.log(query);
+      const { data } = await http.get(
+        "http://localhost:9200/personaldetailss/_search?q=" + query + "&pretty"
+      );
+      const user = data.hits.hits;
+      this.setState({ user });
+    } catch (error) {}
+  };
+
+  get renderUsers() {
+    let message = (
+      <p>Search users by their name, industry, company name etc.</p>
+    );
+    if (this.state.user.length !== 0) {
+      return (
+        <React.Fragment>
+          {this.state.user.map(function(item, i) {
+            return (
+              <React.Fragment>
+                <Link key={item._source._id}>
+                  <p>
+                    {item._source.firstName} {item._source.lastName}
+                  </p>
+                </Link>
+              </React.Fragment>
+            );
+          })}
+        </React.Fragment>
+      );
+    }
+
+    return message;
+  }
+
   render() {
+    const { classes } = this.props;
     return (
       <React.Fragment>
         <br />
         <br />
-        <SearchBox />
-        <SearchCard />
-        <br />
+        <Paper className={classes.paperStyle}>
+          <SearchBox onChange={this.handleChange} value={this.state.query} />{" "}
+          <button>Search</button>
+          <br />
+          {this.renderUsers}
+        </Paper>
+        {/* <SearchCard /> */}
       </React.Fragment>
     );
   }
 }
 
-export default SearchPage;
+export default withStyles(style)(SearchPage);
